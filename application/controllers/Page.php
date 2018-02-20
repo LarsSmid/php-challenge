17d92e02a3da94048c63fbb6f27e3824e03b70f3
Laptop Lars Smid <99039541@mydavinci.nl>
2018-02-20 13:47:03 +0100

    13:46/20-2

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -1,5 +1,5 @@
 <?php
-class Pages extends CI_Controller {
+class Page extends CI_Controller {
 
         public function view($page = 'home')
         {
---
+++ application/views/news/view.php
@@ -0,0 +1,5 @@
+<h1>View News</h1>
+
+<?php
+echo '<h2>'.$news_item['title'].'</h2>';
+echo $news_item['text'];
